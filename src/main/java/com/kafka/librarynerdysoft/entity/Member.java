c93087abd02c9a4e732e82b83666c1c30c26d85a
Oleh Karmazin <kalemzwork@gmail.com>
2025-06-23 18:21:50 +0200

    feat: created entities Book and Member

---
+++ src/main/java/com/kafka/librarynerdysoft/entity/Member.java
@@ -0,0 +1,27 @@
+package com.kafka.librarynerdysoft.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
