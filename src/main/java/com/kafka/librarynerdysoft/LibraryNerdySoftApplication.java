63dbe3e9fb8b2cb028e5e009c345f591c318e784
Oleh Karmazin <kalemzwork@gmail.com>
2025-06-23 18:08:35 +0200

    chore: init commit with basic Spring Boot project

---
+++ src/main/java/com/kafka/librarynerdysoft/LibraryNerdySoftApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.librarynerdysoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryNerdySoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryNerdySoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafka/librarynerdysoft/LibraryNerdySoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.librarynerdysoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryNerdySoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
